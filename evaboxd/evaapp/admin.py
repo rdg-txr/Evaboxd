41ae05c7f7454c0de4acaf0ca40b4a6b5d735c65
Rodrigo Teixeira <45467923+rdg-txr@users.noreply.github.com>
2024-03-27 16:16:36 -0300

    Add files via upload

--- evaboxd/evaapp/admin.py
+++ evaboxd/evaapp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Show, Episode
 
-# Register your models here.
+
+class EpisodeInline(admin.TabularInline):
+    model = Episode
+    extra = 1
+
+
+class ShowAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["show_title", "show_date"]})
+    ]
+    inlines = [EpisodeInline]
+
+
+admin.site.register(Show, ShowAdmin)
--- evaboxd/evaapp/models.py
+++ evaboxd/evaapp/models.py
@@ -2,14 +2,19 @@ from django.db import models
 
 
 class Show(models.Model):
-    show_title = models.CharField(length=200)
-    date_release = models.DateTimeField("date release")
+    show_title = models.CharField(max_length=200)
+    show_date = models.DateTimeField("date release")
 
     def __str__(self):
-        return self.title
+        return self.show_title
 
 
 class Episode(models.Model):
     show = models.ForeignKey(Show, on_delete=models.CASCADE)
-    ep_title = models.CharField(length=200)
-    
+    ep_title = models.CharField(max_length=200)
+    ep_date = models.DateTimeField("date release")
+    ep_season = models.IntegerField(default=0)
+    ep_num = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.ep_title
